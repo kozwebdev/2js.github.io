6af62a152ea12ba0bf383dcfca507aa2570ddaf3
kozwebdev <kozwebdev@gmail.com>
2022-03-29 14:30:00 +0500

    counter

--- js/first.js
+++ js/first.js
@@ -1,2 +1,23 @@
 let message="qroqroqro!";
 alert(message);
+function countToTen()  {
+  var count =  0;
+  while (count < 10)  {
+    count++;
+    console.log(count);
+    document.getElementById("theCount").innerHTML  +=  count + "<br>";
+  }
+}
+/*firstNum = prompt("Введите первое число");
+let secondNum = prompt("Введите второе число");
+let answer  = Number(firstNum) + Number(secondNum);
+alert(answer);*/
+var goat =  new  Array(1,"clap",true);
+
+var cow  = ["moo","troubles"];
+console.log(goat,cow);
+var counter  =  new Array(20);
+for(var i=0; i<counter.length;i++){
+  counter[i]=i;
+  console.log(counter[i]);
+}
